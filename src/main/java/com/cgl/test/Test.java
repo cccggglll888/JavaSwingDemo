209d11526d7b584df0185e6f518e65d3484fe04e
cccggglll888 <361946749@qq.com>
2015-05-18 17:26:46 +0800

    测试git冲突

--- src/main/java/com/cgl/test/Test.java
+++ src/main/java/com/cgl/test/Test.java
@@ -1,5 +1,10 @@
 package com.cgl.test;
 
 public class Test {
-	public String a = "aaaaaa";
+	public static String a = "11";
+	public static String b = "22";
+	
+	public static void main(String[] args) {
+		System.out.println(b+a);
+	}
 }
